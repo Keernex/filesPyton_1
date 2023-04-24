1cb3ea68e5ba8b92706e5f346caf202e985250eb
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:28:18 +0300

    done ex-5

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,8 @@
-
+try:
+    with open("hello.txt", "r") as _file:
+        cont = _file.read()
+        for i in cont:
+            if i=="1" or i=="2" or i=="3" or i=="4" or i=="5" or i=="6" or i=="7" or i=="8" or i=="9" or i=="0":
+                print(i, end=" ")
+except Exception as e:
+    print("Error: " + e)
