6cb909bf4368b18886181bdde52ca2e9c1732040
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:31:47 +0300

    done ex-9

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,9 @@
-
+try:
+    with open("hello.txt", 'r+') as file:
+        cont = file.readlines()
+        sor = sorted(cont)
+        file.seek(0)
+        file.writelines(sor)
+        file.truncate()
+except Exception as e:
+    print("Error: " + e)
