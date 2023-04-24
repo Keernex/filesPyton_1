3f54a2511b200cafd32ad8c52bd4e5ccedc83f25
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:32:47 +0300

    done ex-10

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,10 @@
-
+try:
+    with open("hello.txt", 'r+') as file:
+        cont = file.readlines()
+        q=int(input())
+        cont[q-1] = "" + "\n"
+        print(cont)
+        file.seek(0)
+        file.writelines(cont)
+except Exception as e:
+    print("Error: " + e)
