0d3ed13b4b673cf8fbebf6ebb55ac10c0764bbdc
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:27:33 +0300

    done ex-4

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,8 @@
-
+try:
+    with open("hello.txt", "r", encoding="utf8") as _file:
+        q=input()
+        for line in _file:
+            if q in line:
+                print(line, end="")
+except Exception as e:
+    print("Error: " + e)
