18af30f2c4fa95fc85c52927d9d4a0ddbde105db
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:30:54 +0300

    done ex-8

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,10 @@
-
+try:
+    with open("hello.txt", 'r') as file:
+        cont = file.readlines()
+        q=set()
+        for i in cont:
+            if i in cont:
+                print("Повторення є")
+                break
+except Exception as e:
+    print("Error: " + e)
