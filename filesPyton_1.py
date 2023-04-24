fae57c80c8cceaf0c056205c8e2de9a8c6bff783
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:24:46 +0300

    done ex-2

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,10 @@
-
+try:
+    q=input()
+    with open('hello.txt', 'r') as file:
+        content = file.read()
+        if q in content:
+            print('Рядок знайдено')
+        else:
+            print('Рядок не знайдено')
+except Exception as e:
+    print("Error: " + e)
