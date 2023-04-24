eae1de672ccea695be0373dc89ef769e28d5160f
Maksym Serhiyevich Kushnirenko <100068913+Keernex@users.noreply.github.com>
2023-04-24 22:29:07 +0300

    done ex-6

--- filesPyton_1.py
+++ filesPyton_1.py
@@ -1,1 +1,8 @@
-
+try:
+    with open("hello.txt", "r") as _file:
+        q=int(input())
+        cont = _file.readlines()
+        for i in range(q*-1,-1+1):
+            print(cont[i],end="")
+except Exception as e:
+    print("Error: " + e)
